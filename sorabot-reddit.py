058ec10bb88efc407e1a7bb130175027a54ea4db
zk00332 <zk00332@surrey.ac.uk>
2023-08-21 00:35:11 +0100

    added changes

--- sorabot-reddit.py
+++ sorabot-reddit.py
@@ -2,6 +2,12 @@ import praw
 import random
 import time
 import os
+import numpy as np
+from transformers import pipeline
+import statistics as stats
+from itertools import islice
+
+from nltk.sentiment import SentimentIntensityAnalyzer
 
 # https://www.reddit.com/prefs/apps
 
@@ -16,6 +22,23 @@ class Bot():
         self.samples = samples
         self.subreddit_name = subreddit_name
     
+    def sentiment(self, buffer):
+        stream = SentimentIntensityAnalyzer()
+        analysis = ""
+        for e in range(len(buffer)):
+            analysis = stream.polarity_scores(str(e))
+        return "Positive:", analysis["pos"], "Negative:", analysis["neg"]
+
+
+    def villain(self, string):
+        stream = SentimentIntensityAnalyzer()
+        analysis = stream.polarity_scores(string)
+        
+        pos_score = analysis["pos"]
+        neg_score = analysis["neg"]
+
+        return tuple((pos_score, neg_score)) 
+
     def run(self):
         reddit = praw.Reddit(
         client_id= self.client_id,
@@ -26,23 +49,61 @@ class Bot():
         subreddit_channel = reddit.subreddit(self.subreddit_name)
         print(subreddit_channel)
         
+
+        x = None
         i = 1
         for post in subreddit_channel.hot(limit=self.samples):
-            print("---------------------------------" + "\n")
-            print("POST " + str(i) + " " + str(post.title))
-            print("Link: " + str(post.url) + "\n") # grabbing the title
+            # print("---------------------------------" + "\n")
+            # print("POST " + str(i) + " " + str(post.title))
+            # print("Link: " + str(post.url) + "\n") # grabbing the title
 
             for comment in post.comments:
                 if hasattr(comment, "body"):
                     comment_lower = comment.body.lower()
-                    print("-------------------------------")
-                    print("Comment: " + str(comment.body)) 
+                    #print("-------------------------------")
+                    #print("Comment: " + str(comment.body)) 
+
+                    comment_feed = []            
+                    comment_feed.append(comment_lower)
+                    
+
+            buffer = [(self.villain(comment.body.lower())) for comment in post.comments if
+                                hasattr(comment,"body")]
+            
+            pos_prediction_buffer = []
+            neg_prediction_buffer = []
+
+            print(buffer ,"\n")
+            for i in range(len(buffer)):
+                for j in buffer[i-1]:
+                    pos_prediction_buffer.append(j)
+                    neg_prediction_buffer.append(j+1)
+
+            print()
+            print("Statistics -> Positive: " +
+            str(stats.mean(pos_prediction_buffer))
+            + " Negative: " + str(stats.mean(neg_prediction_buffer)))
+
+            # print(self.sentiment(buffer))
+        
             i += 1
-            time.sleep(10)
+            time.sleep(5)
+
+       # for i in range(len(comment_feed)):
+            # prediction_buffer.append(stats.mean(self.sentiment(comment_feed[i])))
+
+
+        return 0
+
+        
             
 if __name__=="__main__":
-    bot = Bot(client_id="", client_secret=""
-               user_agent="",
-               username="", samples=0, subreddit_name="")
+    bot = Bot(client_id="4u59Mj03AIwY0R55ABs_FQ",
+              client_secret="pGB-myBerIjoGk6m57cof6vQH9BOrw",
+              user_agent="<client:SORA-BOT:1.0>",
+              username="Illustrious_Park7068", samples=3, subreddit_name="kpop")
 
-    bot.run()
+    arr = ["hello i hate you!", "hello, i dont hate you!", "lol", "bog", "h"]
+    print(bot.run())
+   # print(bot.sentiment(arr))
+
